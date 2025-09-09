eb13cd9d4a002806b825df66177fe7b0155c17b7
Vadim Rasstrigin <Rasstriginv@gmail.com>
2025-09-09 12:55:02 +0200

    chat styles

--- src/store/atoms/userName/atom.ts
+++ src/store/atoms/userName/atom.ts
@@ -1,6 +1,6 @@
 import i18next from 'i18next';
 import { atomWithStorage } from 'jotai/utils';
 
-const DEFAULT_USER_NAME = i18next.t('layout.visitor');
+const DEFAULT_USER_NAME = i18next.t('layout.visitor') || 'Visitor';
 
 export const UserName = atomWithStorage<string>('chatUserName', DEFAULT_USER_NAME);
