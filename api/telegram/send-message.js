2d1417473d372cb613ac0de49a0ffa31190011ff
Vadim Rasstrigin <Rasstriginv@gmail.com>
2025-09-10 18:52:07 +0200

    fixes

--- api/telegram/send-message.js
+++ api/telegram/send-message.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
 }
 
 function formatMessageForTelegram(message, userMetadata) {
-  let formattedMessage = `💬 \n${message}\n\n`;
+  let formattedMessage = `💬 \n 📝 ${message}\n\n`;
 
   if (userMetadata) {
     // User info
